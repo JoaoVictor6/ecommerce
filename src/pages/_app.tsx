b0fc9ca114be0ff29d0b11f9d7b939414aa0bc41
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-03-29 16:21:22 -0300

    fix: realocate files and resolve routes conflict between miragejs and next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@styles/global.css';
 import type { AppProps } from 'next/app';
 
+import { makeServer } from '@/mirage';
+
+if (process.env.NODE_ENV !== 'production') {
+  makeServer();
+}
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
